bc4ade15193448d73fbec4f01e970c7797964b58
Calyhre <calyhre@gmail.com>
2014-04-21 18:03:42 +0200

    Add Worker specs

--- lib/waffle_api/worker.rb
+++ lib/waffle_api/worker.rb
@@ -3,9 +3,9 @@ module WaffleAPI
   class Worker
     attr_reader :name, :hash_rate, :stale_rate, :last_seen
 
-    def initialize(name:, hash_rate:, stale_rate:, last_seen:)
-      @name = name.split('_')[1..-1].join
-      @name = name if @name.empty?
+    def initialize(name: '', hash_rate: 0, stale_rate: 0.0, last_seen: 0)
+      @name = name.split('_', 2).last
+      @name = name if @name.nil? || @name.empty?
       @hash_rate = hash_rate
       @stale_rate = stale_rate
       @last_seen = Time.at(last_seen)
