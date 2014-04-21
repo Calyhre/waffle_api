82ca40fb4e42ecd3d88faf8f5d0f60bf0c40b1da
Calyhre <calyhre@gmail.com>
2014-04-21 02:34:46 +0200

    Add client_spec

---
+++ spec/lib/waffle_api/client_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe WaffleAPI::Client do
+  describe 'without address' do
+    it 'raises an WaffleAPI::Error::EmptyAddress' do
+      expect do
+        WaffleAPI::Client.new
+      end.to raise_error(WaffleAPI::Error::EmptyAddress)
+    end
+  end
+
+  describe 'with a bad address' do
+    it 'raises an WaffleAPI::Error::BadAddress' do
+      expect do
+        WaffleAPI::Client.new address: 'some_bad_address'
+      end.to raise_error(WaffleAPI::Error::BadAddress)
+    end
+  end
+
+  describe 'with a valid address' do
+    subject do
+      WaffleAPI::Client.new address: '1Ju8U9Ukfc5kiMqzQrRgQBP1JvRkeSv94V'
+    end
+
+    describe 'asking for a non existing key' do
+      it 'raises an NoMethodError' do
+        stub_waffle Wafflepool::Success
+
+        expect do
+          subject.i_hope_this_key_will_never_exist
+        end.to raise_error(NoMethodError)
+      end
+    end
+
+    describe 'asking for an existing key' do
+      it 'just return it' do
+        stub_waffle Wafflepool::Success
+
+        expect(subject.hashrate).to eq 4_303_407
+      end
+    end
+  end
+end
